86ede739d91b91956ab55391103ac87fa83c2eb9
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-07-28 13:54:49 -0300

    Exercicio operadores JS -falta o 3º-

---
+++ semana-3/operadores/index.js
@@ -0,0 +1,53 @@
+//1.
+    //A.  false
+
+    //B.  false
+
+    //C.  false
+
+    //E.  boolean
+
+//2.
+
+    //A.  undefined
+    //B.  null
+    //C.  11
+    //D.  0
+    //E.  19
+    //F.  25
+
+
+// ------------------------------------
+
+//1
+
+// let idade = prompt("Qual a sua idade?")
+// let idadeAmigo = prompt("Qual a idade da sua melhor amiga?")
+// let comparacaoIdade = console.log("Sua idade é maior do que a do seu amigo?", idade > idadeAmigo)
+
+// let sumDireferenca = console.log("Diferença de", Number(idade - idadeAmigo), "anos")
+
+//2
+
+let evenNumber = Number(prompt("Porfavor, insira um número par:"))
+
+console.log(evenNumber % 2)
+    //c. Sim, todo número par dividido por 2 dá modulo 0
+    //d. Sim, como a divisão é "quebrada" dá modulo 1.
+
+//3
+
+// let listaDeTarefas = []
+
+// let tarefasAFazer = Array(prompt("Me conte 1 tarefas que tu precisa realizar hoje:"))
+
+// let addArrayItems = listaDeTarefas.push(tarefasAFazer[i])
+
+// console.log(addArrayItems)
+
+//4
+
+let name = prompt("Qual o seu nome?")
+let email = prompt("Qual seu email?")
+
+console.log("O e-mail", email, "foi cadastrado com sucesso. Seja bem-vinde,", name, "!")
