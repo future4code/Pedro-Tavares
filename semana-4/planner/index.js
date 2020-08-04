8dfb300269f96efa78f289cce84bdee9e4a4cf27
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-08-04 18:15:35 -0300

    correcao da function addTarefa

--- semana-4/planner/index.js
+++ semana-4/planner/index.js
@@ -1,18 +1,39 @@
-function addTarefa(){
+function addTarefa() {
     const inputTarefa = document.getElementById("tarefa")
     let valorInputTarefa = inputTarefa.value
+    let valorDiasSemana = document.getElementById("dias-semana").value
+    let diaEscolhido
 
-    let diasDaSemana = document.getElementById('dias-semana')
-    
-    let valorDiasDaSemana = diasDaSemana.value
-    let listaDiaSemana = document.getElementsByClassName('semana')
-    let valorListaDiaSemana = listaDiaSemana.value
-    switch(valorDiasDaSemana){
-        case "Domingo":
-            valorDiasDaSemana = valorListaDiaSemana.innerHtml += `<li>${valorInputTarefa}</li>`
+    switch (valorDiasSemana) {
+        case "domingo":
+            diaEscolhido = document.getElementById("domingo")
+            diaEscolhido.innerHTML += `<p>${valorInputTarefa}</p>`
+            break
+        case "segunda":
+            diaEscolhido = document.getElementById("segunda")
+            diaEscolhido.innerHTML += `<p>${valorInputTarefa}</p>`
+            break
+        case "terca":
+            diaEscolhido = document.getElementById("terca")
+            diaEscolhido.innerHTML += `<p>${valorInputTarefa}</p>`
+            break
+        case "quarta":
+            diaEscolhido = document.getElementById("quarta")
+            diaEscolhido.innerHTML += `<p>${valorInputTarefa}</p>`
+            break
+        case "quinta":
+            diaEscolhido = document.getElementById("quinta")
+            diaEscolhido.innerHTML += `<p>${valorInputTarefa}</p>`
+            break
+        case "sexta":
+            diaEscolhido = document.getElementById("sexta")
+            diaEscolhido.innerHTML += `<p>${valorInputTarefa}</p>`
+            break
+        case "sabado":
+            diaEscolhido = document.getElementById("sabado")
+            diaEscolhido.innerHTML += `<p>${valorInputTarefa}</p>`
+            break            
     }
 
-
-    
     inputTarefa.value = ""
 }
